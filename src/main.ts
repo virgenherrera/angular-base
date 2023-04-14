3e2ee96ec7dfdffd864ac088c9a1873342e4267a
Hugo Virgen <virgenherrera@gmail.com>
2023-04-14 13:49:18 -0600

    feat: migration to V15 (#141)
    * feat: migration to V15
    
    - instal dependencies.
    - update/drop files.
    - implement eslint-fix only.
    - drop (check:) prettier from cy.yml

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,7 @@
-import { enableProdMode, ViewEncapsulation } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule, {
-    defaultEncapsulation: ViewEncapsulation.ShadowDom,
-  })
+  .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
